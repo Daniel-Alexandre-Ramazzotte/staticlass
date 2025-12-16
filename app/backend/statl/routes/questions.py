11228733b095158f5cc587d9c690784a5342fdd7
ViniOKD <vinitokada@gmail.com>
2025-12-16 18:53:06 -0300

    remove comments

--- app/backend/statl/routes/questions.py
+++ app/backend/statl/routes/questions.py
@@ -31,27 +31,15 @@ def check_correct_answer():
 @bp.route('/add', methods = ['POST'])
 def add_question():
     data: Dict[str, Any] = request.form.to_dict()
-    print("\n--- DEBUG DO HEADER ---")
-    content_type = request.headers.get('Content-Type')
-    print(f"1. Content-Type recebido: {content_type}")
     
-    # Verifica o tamanho do pacote (bytes)
-    # Se for 0, o Postman não mandou nada.
-    # Se for grande (>1000), o arquivo chegou mas o Flask não leu.
-    raw_data = request.get_data()
-    print(f"2. Tamanho total dos dados: {len(raw_data)} bytes")
-    
-    print(f"3. Chaves de Arquivo: {request.files.keys()}")
-    print("-----------------------\n")
     file_issue = request.files.get("image_q")
     file_solution = request.files.get("image_s")
 
     path_issue = process_upload(file_issue)
-
     path_solution = process_upload(file_solution)
  
 
     data['image_q'] = path_issue
     data['image_s'] = path_solution
-    print(data)
+
     return add_question_service(data)
